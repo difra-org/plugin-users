926641e880cd451bea5c6d1afa0820fc837c4e37
Eduard Kutuev <nap@napych.com>
2017-01-20 15:29:41 +0300

    added sources

---
+++ Plugin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Difra\Plugins\Users;
+
+use Difra\Events;
+
+/**
+ * Class Plugin
+ * @package Difra\Plugins\Users
+ */
+class Plugin extends \Difra\Plugin
+{
+    protected $provides = 'auth';
+    protected $require = ['database','captcha'];
+    protected $version = 7;
+    protected $description = 'User accounts';
+
+    public function init()
+    {
+        Events::register(Events::EVENT_CONFIG_LOAD, '\Difra\Plugins\Users\Session', 'load');
+    }
+}
